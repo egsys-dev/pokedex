eca969306ec7fa71a28f46f99370a97b668d1f9a
wellyngson <wellyngson.guedes@gmail.com>
2022-04-03 14:52:22 -0300

    Add pokemon model

---
+++ app/src/main/java/br/egsys/pokedex/data/model/Pokemon.kt
@@ -0,0 +1,34 @@
+package br.egsys.pokedex.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val sprites: Sprites,
+    val types: Types,
+    val weight: Int,
+    val height: Int
+)
+
+data class Sprites(
+    val other: Other
+)
+
+data class Other(
+    @SerializedName("dream_world")
+    val dreamWorld: FrontDefault
+)
+
+data class FrontDefault(
+    @SerializedName("front_default")
+    val frontDefault: String
+)
+
+data class Types(
+    val type: Type
+)
+
+data class Type(
+    val name: String
+)
